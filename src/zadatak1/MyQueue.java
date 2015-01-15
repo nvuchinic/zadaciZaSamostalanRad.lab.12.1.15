5bfb5d49fd9b8c5684b5f7642dbaf8f9725c77b1
nvuchinic <nermin.vucinic@gmail.com>
2015-01-16 00:23:04 +0100

    Update MyQueue.java

--- src/zadatak1/MyQueue.java
+++ src/zadatak1/MyQueue.java
@@ -1,18 +1,27 @@
 package zadatak1;
-
+/**
+ * class consisting of two stacks simulating the queue data structure
+ * */
 public class MyQueue {
 	StackInt stack1;
 	StackInt stack2;
 
+/**
+ * constructor method for creating MyQueue objects
+ * */
 	public MyQueue() {
 		stack1 = new StackInt();
 		stack2 = new StackInt();
 	}
-
-	public void push(int value) {
+/**
+ * inserts value into stack1 
+ * */
+ 	public void push(int value) {
 		stack1.push(value);
 	}
-
+/**
+ * loads queue(insert elements from stack1 to stack2)
+ * */
 	public void loadTheQueue() {
 		int size = stack1.getSize();
 		while (size > 0) {
@@ -20,7 +29,9 @@ public class MyQueue {
 			size--;
 		}
 	}
-
+/**
+ * returns and removes element from the queue
+ * */
 	public int popMyQueue() {
 		if (stack2.isEmpty() == true) {
 			if (stack1.isEmpty() == true) {
@@ -35,7 +46,9 @@ public class MyQueue {
 			return stack2.pop();
 		}
 	}
-
+	/**
+	 * prints the elements of queue
+	 * */
 	public String printMyQueue() {
 		if (!stack2.isEmpty()) {
 			return stack2.printStack();
